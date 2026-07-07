d8633dd826ed742c1527907c960406215818d32f
Harshit Kumar <harshitsv2005@gmail.com>
2026-07-08 01:17:36 +0530

    Add Chapter 05 Dictionary basics

--- CHAPTER-05/03_values_method.py
+++ CHAPTER-05/03_values_method.py
@@ -1,7 +1,18 @@
+# student = {
+#     "name": "Harshit",
+#     "age": 21,
+#     "city": "Meerut"
+# }
+
+# print(student.values())
+
+
 student = {
     "name": "Harshit",
     "age": 21,
     "city": "Meerut"
 }
 
-print(student.values())
+all_values = student.values()
+
+print(all_values)
