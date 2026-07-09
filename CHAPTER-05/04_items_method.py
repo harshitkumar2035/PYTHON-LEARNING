72eab97e76a3cf85711fed4a2b616e97d2c497be
Harshit Kumar <harshitsv2005@gmail.com>
2026-07-09 13:50:21 +0530

    Add Chapter 05 Dictionary basics

--- CHAPTER-05/04_items_method.py
+++ CHAPTER-05/04_items_method.py
@@ -7,12 +7,22 @@
 # print(student.items())
 
 
+# student = {
+#     "name": "Harshit",
+#     "age": 21,
+#     "city": "Meerut"
+# }
+
+# all_items = student.items()
+
+# print(all_items)
+
+
 student = {
     "name": "Harshit",
     "age": 21,
     "city": "Meerut"
 }
 
-all_items = student.items()
-
-print(all_items)
+for item in student.items():
+    print(item)
