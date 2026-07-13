54cd42930f7bd77872b3b8115aa245f5a9321743
Harshit Kumar <harshitsv2005@gmail.com>
2026-07-13 21:58:11 +0530

    Add Chapter 05 Dictionary basics

--- CHAPTER-05/08_popitem_method.py
+++ CHAPTER-05/08_popitem_method.py
@@ -1,10 +1,20 @@
+# student = {
+#     "name": "Harshit",
+#     "age": 21,
+#     "city": "Meerut"
+# }
+
+# deleted_item = student.popitem()
+
+# print("Deleted Item :", deleted_item)
+# print(student)
+
 student = {
     "name": "Harshit",
     "age": 21,
-    "city": "Meerut"
+    "city": "Meerut",
+    "course": "Python"
 }
 
-deleted_item = student.popitem()
-
-print("Deleted Item :", deleted_item)
+print(student.popitem())
 print(student)
